568d4e22e29b37047c97cb705862da5435d4956e
zhangbaokun <zhangbaokun@corp.netease.com>
2016-12-24 12:28:11 +0800

    增加测试界面

---
+++ app/src/main/java/com/example/zbk/demo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.zbk.demo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.example.zbk.fragment.MainFragment;
+
+public class MainActivity extends AppBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_common);
+    }
+
+    @Override
+    public Fragment createCommonFragment() {
+        return new MainFragment();
+    }
+}
